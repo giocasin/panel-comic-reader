6d30aa4f809f97fbc09bfa1364b3b02a7ec5af6e
Gioele <gioele.gg@gmail.com>
2023-09-02 14:55:03 +0200

    added Full View Mode

--- src/components/ComicPageComponent/ComicPageComponent.js
+++ src/components/ComicPageComponent/ComicPageComponent.js
@@ -8,12 +8,14 @@ export const ComicPage = ({isManga, comic, closeComic}) => {
     const [currentFirstPage, setCurrentFirstPage] = useState(comic.images[currentFirstPageIndex].data.src);
     const [currentSecondPage, setCurrentSecondPage] = useState(comic.images.length > 1 ? comic.images[currentSecondPageIndex].data.src : undefined);
     const [zoomedView, setZoomedView] = useState(false);
+    const [fullView, setFullView] = useState(false);
     
     const refFirstImage = useRef('firstImage');
     const refSecondImage = useRef('secondImage');
 
     useEffect(() => {
         setZoomedView(false);
+        setFullView(false);
     }, []);
 
     useEffect(() => {
@@ -22,7 +24,7 @@ export const ComicPage = ({isManga, comic, closeComic}) => {
     },  [currentFirstPageIndex, currentSecondPageIndex, comic.images]);
 
     const goNextPage = () => {
-        if (currentSecondPageIndex + 1 < comic.images.length ) {
+        if (!fullView && currentSecondPageIndex + 1 < comic.images.length ) {
             setCurrentFirstPageIndex(currentFirstPageIndex + 1);
             setCurrentSecondPageIndex(currentSecondPageIndex + 1);
         }
@@ -30,7 +32,7 @@ export const ComicPage = ({isManga, comic, closeComic}) => {
     }
 
     const goPrevPage = () => {
-        if (currentFirstPageIndex > 0) {
+        if (!fullView && currentFirstPageIndex > 0) {
             setCurrentFirstPageIndex(currentFirstPageIndex - 1);
             setCurrentSecondPageIndex(currentSecondPageIndex - 1);
         }
@@ -52,16 +54,34 @@ export const ComicPage = ({isManga, comic, closeComic}) => {
         <div className={'opened-comic'} onKeyDown={(e) => arrowsNavigation(e)}>
             <div className={'comic-navigation'}>
                 <button onClick={() => closeComic()}>Close</button>
-                <button onClick={() => setZoomedView(!zoomedView)}>Zoomed View</button>
-                <button onClick={() => goNextPage()}>Next</button>
-                <button onClick={() => goPrevPage()}>Previous</button>
+                {!fullView && 
+                    <span>
+                        <button onClick={() => setZoomedView(!zoomedView)}>Zoomed View</button>
+                        <button onClick={() => goNextPage()}>Next</button>
+                        <button onClick={() => goPrevPage()}>Previous</button>
+                    </span>
+                }
+                <button onClick={() => {setFullView(!fullView);}}>Full View</button>
             </div>
-            <div className={'pages-container'}>
-                <img ref={refFirstImage} src={isManga ? currentSecondPage : currentFirstPage} style={{width: zoomedView === true ? '100%' : '50%', maxWidth: zoomedView === true ? '100%' : '50%', display: isManga && zoomedView === true ? 'none' : 'inline'}}
-                    onClick={isManga ? () => goNextPage() : () => goPrevPage()} onSelect={() => {}} alt='comic page'/>
+            <div className={'pages-container'} style={{display: fullView === true ? 'block' : 'block ruby'}}>
+                {!fullView ? 
+                    ( 
+                        <span>
+                            <img ref={refFirstImage} src={isManga ? currentSecondPage : currentFirstPage} style={{width: zoomedView === true ? '100%' : '50%', maxWidth: zoomedView === true ? '100%' : '50%', display: isManga && zoomedView === true ? 'none' : 'inline'}}
+                            onClick={isManga ? () => goNextPage() : () => goPrevPage()} onSelect={() => {}} alt='comic page'/>
 
-                <img ref={refSecondImage} src={isManga ? currentFirstPage : currentSecondPage} style={{width: zoomedView === true ? '100%' : '50%', maxWidth: zoomedView === true ? '100%' : '50%', display: !isManga && zoomedView === true ? 'none' : 'inline'}}
-                onClick={isManga ? () => goPrevPage() : () => goNextPage()} onSelect={() => {}} alt='comic page'/> 
+                            <img ref={refSecondImage} src={isManga ? currentFirstPage : currentSecondPage} style={{width: zoomedView === true ? '100%' : '50%', maxWidth: zoomedView === true ? '100%' : '50%', display: !isManga && zoomedView === true ? 'none' : 'inline'}}
+                            onClick={isManga ? () => goPrevPage() : () => goNextPage()} onSelect={() => {}} alt='comic page'/> 
+                        </span>
+                    )   :
+                    (
+                        comic.images.map(image => {
+                            return (
+                                <img ref={image} src={image.data.src} style={{width:'70%', maxWidth: '70%', display: 'block', margin: '0 auto'}} alt='comic page'/>
+                            );
+                        })
+                    )
+                }
             </div>
         </div>
     );
